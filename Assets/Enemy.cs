c795f38c7f81c07584cabba8573a022d5313253e
Jeroen van der Last <jeroen.vander.last@essent.nl>
2020-04-02 15:25:41 +0200

    Finish step 2

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        waypointIndex++;
+        if (waypointIndex >= Waypoints.waypoints.Length)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        target = Waypoints.waypoints[waypointIndex];
+    }
+}
